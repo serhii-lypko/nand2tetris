e9e7b31f729aef4a5e52c9bb61295730e6fcf12b
Serhii Lypko <lypko.sg@gmail.com>
2025-02-24 09:32:17 +0000

    Machine language: infinite loop counter

---
+++ machine-language/vars_infinite_inc.asm
@@ -0,0 +1,27 @@
+// set @current variable to value of register 0
+@R0
+D=M
+@current
+M=D
+
+// set @current variable to value of register 1
+@R1
+D=M
+@step
+M=D
+
+// could be named as (LOOP)
+(START) 
+  @current
+  D=M
+  @step
+  D=D+M
+
+  @current
+  M=D
+
+  @R2
+  M=D
+
+  @START   
+  0;JMP
